dc6ff7781e7364e6bc137fd1a506ad9c4dc8a08c
Diego Henrique <diegohrp20@gmail.com>
2022-02-22 16:09:07 -0300

    adding authentication middleware for private controllers

--- sisrepo-server/src/routes.js
+++ sisrepo-server/src/routes.js
@@ -1,11 +1,20 @@
 import { Router } from "express";
+
+import auth from "./middlewares/auth";
+
 import HelloController from "./controllers/HelloController";
 import RepositoriesController from "./controllers/RepositoriesController";
 import UsersController from "./controllers/UsersController";
 
 const routes = new Router();
 
+// controller publico 
 routes.get('/hello', HelloController.index);
+
+// middleware
+routes.use(auth);
+
+// controllers privados
 
 routes.get('/users', UsersController.index);
 routes.get('/users/:id', UsersController.show);
