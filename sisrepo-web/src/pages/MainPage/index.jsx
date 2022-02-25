7ecd551d6246182581e58b9b84d0c987ea4bdba1
Diego Henrique <diegohrp20@gmail.com>
2022-02-25 19:52:33 -0300

    Structuring the Repository Menu Main Page Layout

--- sisrepo-web/src/pages/MainPage/index.jsx
+++ sisrepo-web/src/pages/MainPage/index.jsx
@@ -3,7 +3,19 @@ import "./styles.css";
 
 const MainPage = () => {
     const handleLogout = () => {
-        console.log('logout');
+        console.log('Logout');
+    }
+
+    const handleSearch = (query) => {
+        console.log('Search: ',query);
+    }
+
+    const handleClear = () => {
+        console.log('Clear');
+    }
+
+    const handleDeleteRepo = () => {
+        console.log('Delete Repo');
     }
 
     return (
@@ -12,6 +24,41 @@ const MainPage = () => {
                 <h1 className="logo">SisRepo</h1>
 
                 <button onClick={handleLogout}>Sair</button>
+            </div>
+
+            <div className="search">
+                <label htmlFor="query">Procurar: </label>
+                <input type="search" name="query" id="query" />
+                <button onClick={handleClear}>Limpar</button>
+                <button onClick={handleSearch}>Procurar</button>
+            </div>
+
+            <div className="repositories">
+                <h2 className="title">Repositórios</h2>
+
+                <ul className="list">
+                    <li className="item">
+                        <div className="info">
+                            <div className="owner">facebook</div>
+                            <div className="name">react</div>
+                        </div>
+                        <button onClick={handleDeleteRepo}>Apagar</button>
+                    </li>
+
+                    <li className="item">
+                        <div className="info">
+                            <div className="owner">facebook</div>
+                            <div className="name">react</div>
+                        </div>
+                        <button onClick={handleDeleteRepo}>Apagar</button>
+                    </li>
+                </ul>
+
+                <div className="new">
+                    <label htmlFor="new-repo">Novo Repositório: </label>
+                    <input type="url" name="new-repo" id="new-repo" />
+                    <button onClick={handleClear}>Adicionar</button>
+                </div>
             </div>
         </div>
     )
