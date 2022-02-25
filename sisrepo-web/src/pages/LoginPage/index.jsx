9dcb00db2c51a668c7cd8b2de3454fffe20f1673
Diego Henrique <diegohrp20@gmail.com>
2022-02-25 02:31:13 -0300

    Capturing the events built on the Login screen using the useState that is part of the hooks structure.

--- sisrepo-web/src/pages/LoginPage/index.jsx
+++ sisrepo-web/src/pages/LoginPage/index.jsx
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     const handleLogin = () => {
         console.log('login');
+        console.log('Email: ', email);
+        console.log('Password: ', password);
     }
+
     return (
         <div id="login">
             <h1 className="title">Login</h1>
             <div className="form">
                 <div className="field">
                     <label htmlFor="email">Email:</label>
-                    <input type="email" name="email" id="email" />
+                    <input 
+                    type="email" 
+                    name="email" 
+                    id="email" 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
 
                 <div className="field">
                     <label htmlFor="password">Senha:</label>
-                    <input type="password" name="password" id="password" />
+                    <input 
+                    type="password" 
+                    name="password" 
+                    id="password" 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
 
                 <div className="actions">
