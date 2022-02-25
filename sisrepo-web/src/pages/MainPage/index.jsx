df09c7a88dcddc7897618764b11ea093935c2b7d
Diego Henrique <diegohrp20@gmail.com>
2022-02-25 11:08:48 -0300

    Creating the Logout navbar

--- sisrepo-web/src/pages/MainPage/index.jsx
+++ sisrepo-web/src/pages/MainPage/index.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import "./styles.css";
 
-const MainPage = () => (
-    <h1>Main Page</h1>
-)
+const MainPage = () => {
+    const handleLogout = () => {
+        console.log('logout');
+    }
+
+    return (
+        <div id="main">
+            <div className="nav">
+                <h1 className="logo">SisRepo</h1>
+
+                <button onClick={handleLogout}>Sair</button>
+            </div>
+        </div>
+    )
+}
 
 export default MainPage;
