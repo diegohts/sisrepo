40fa70a02b923b18c92ab77834aedc93bf482125
Diego Henrique <diegohrp20@gmail.com>
2022-02-25 02:22:08 -0300

    Configuring the login screen customization part.

--- sisrepo-web/src/pages/LoginPage/index.jsx
+++ sisrepo-web/src/pages/LoginPage/index.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./styles.css";
 
-const LoginPage = () => (
-    <h1>Login Page</h1>
-)
+const LoginPage = () => {
+    const handleLogin = () => {
+        console.log('login');
+    }
+    return (
+        <div id="login">
+            <h1 className="title">Login</h1>
+            <div className="form">
+                <div className="field">
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" name="email" id="email" />
+                </div>
+
+                <div className="field">
+                    <label htmlFor="password">Senha:</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+
+                <div className="actions">
+                    <button onClick={handleLogin}>Entrar</button>
+                </div>
+            </div>
+        </div>
+    )
+};
 
 export default LoginPage;
